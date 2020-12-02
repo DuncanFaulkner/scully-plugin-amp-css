eb6d3dd8d1c79dbb1ddc9e7cd78e40303666d502
Preston Lamb <preston.j.lamb@gmail.com>
2020-12-01 21:07:44 -0700

    docs: ✏️ add documentation for plugin
    Add full documentation for the scully-plugin-amp-css, and updated the
    logo
    
    Closes: #1

--- scully.scully-plugin-amp-css.config.ts
+++ scully.scully-plugin-amp-css.config.ts
@@ -1,14 +1,6 @@
 import { HandledRoute, registerPlugin, ScullyConfig } from '@scullyio/scully';
 require('@scullyio/scully-plugin-amp-css');
 
-// const combineStylesAmpPlugin = async (html: any, route: HandledRoute) => {
-// 	console.log('hello there');
-// 	console.log(html);
-// 	console.log(route);
-// };
-// const validator = () => [];
-// registerPlugin('render', 'testPlugin', combineStylesAmpPlugin, validator);
-
 export const config: ScullyConfig = {
 	projectRoot: './src',
 	projectName: 'scully-plugin-amp-css',
