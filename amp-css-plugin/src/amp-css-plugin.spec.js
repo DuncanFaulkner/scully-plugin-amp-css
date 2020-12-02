f3ffe6124ea82cbcd64eaf4717ed325a296bc44e
Preston Lamb <preston.j.lamb@gmail.com>
2020-12-01 21:45:16 -0700

    test: 💍 added jest test for plugin
    Added a jest test for the main plugin function, but need to figure out
    how to run it properly
    
    Closes: #3

---
+++ amp-css-plugin/src/amp-css-plugin.spec.js
@@ -0,0 +1,13 @@
+const { combineStylesAmpPlugin } = require('./amp-css-plugin.js');
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
+
+const STARTING_HTML = `<!DOCTYPE html> <html lang="en"> <head> <style>html { background: #dedede; }</style> <style>html { color: #444; }</style> <style>* { font-family: 'Arial'; }</style> <title>Test HTML File</title> </head> <body> </body> </html>`;
+
+const EXPECTED_HTML = `<!DOCTYPE html><html lang=\"en\"><head>    <title>Test HTML File</title> <style>html { background: #dedede; }html { color: #444; }* { font-family: 'Arial'; }</style></head> <body>  </body></html>`;
+
+test('it should convert multiple style tags into one style tag', async () => {
+	const newHtml = await combineStylesAmpPlugin(STARTING_HTML, { route: 'test-route' });
+
+	expect(newHtml).toBe(EXPECTED_HTML);
+});
